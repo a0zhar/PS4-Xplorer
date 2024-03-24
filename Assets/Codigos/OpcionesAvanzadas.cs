6f232c5bbbaa1035ef9c503c446f37a9e3d990d9
a0zhar <96749372+a0zhar@users.noreply.github.com>
2024-03-24 17:10:53 +0100

    Update OpcionesAvanzadas.cs

--- Assets/Codigos/OpcionesAvanzadas.cs
+++ Assets/Codigos/OpcionesAvanzadas.cs
@@ -1,26 +1,22 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class OpcionesAvanzadas : MonoBehaviour {
-
     private int Opcion = 0;
     private bool Paso = true;
 
     public GameObject[] MisOpciones = new GameObject[2];
 
-    public void OnDisable()
-    {
+    public void OnDisable() {
         Opcion = 0;
         MisOpciones[0].transform.GetChild(0).gameObject.SetActive(true);
         MisOpciones[1].transform.GetChild(0).gameObject.SetActive(false);
         //MisOpciones[2].transform.GetChild(0).gameObject.SetActive(false);
     }
 
-    void Update()
-    {
-        if ((Input.GetAxis("dpad1_vertical") > 0 || Input.GetKey(KeyCode.UpArrow)) && Opcion > 0 && Paso)
-        {
+    private void Update() {
+        if ((Input.GetAxis("dpad1_vertical") > 0 || Input.GetKey(KeyCode.UpArrow)) && Opcion > 0 && Paso) {
             Paso = false;
 
             MisOpciones[Opcion].transform.GetChild(0).gameObject.SetActive(false);
@@ -30,8 +26,7 @@ public class OpcionesAvanzadas : MonoBehaviour {
             StartCoroutine(SeguirPasando());
         }
 
-        if ((Input.GetAxis("dpad1_vertical") < 0 || Input.GetKey(KeyCode.DownArrow)) && Opcion < 1 && Paso)
-        {
+        if ((Input.GetAxis("dpad1_vertical") < 0 || Input.GetKey(KeyCode.DownArrow)) && Opcion < 1 && Paso) {
             Paso = false;
 
             MisOpciones[Opcion].transform.GetChild(0).gameObject.SetActive(false);
@@ -41,25 +36,20 @@ public class OpcionesAvanzadas : MonoBehaviour {
             StartCoroutine(SeguirPasando());
         }
 
-        if (Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKeyDown(KeyCode.Keypad2))
-        {
-            switch (Opcion)
-            {
-                case 0:
-                    Controlador.instancia.ActivarFTP();
-                    break;
-                case 1:
-                    Controlador.instancia.ActivarFullRW();
-                    break;
-                //case 2:
-                //    Controlador.instancia.ExportarPFS_Map();
-                //    break;
+        if (Input.GetKeyDown(KeyCode.Joystick1Button0) || 
+            Input.GetKeyDown(KeyCode.Keypad2)) {
+            if (Opcion == 0) {
+                Controlador.instancia.ActivarFTP();
+            } else if (Opcion == 1) {
+                Controlador.instancia.ActivarFullRW();
             }
+            //case 2:
+            //    Controlador.instancia.ExportarPFS_Map();
+            //    break;
         }
     }
 
-    private IEnumerator SeguirPasando()
-    {
+    private IEnumerator SeguirPasando() {
         yield return new WaitForSeconds(0.2f);
         Paso = true;
     }
