754c376fbc43422cdfa08f34986ca3054e35871d
a0zhar <96749372+a0zhar@users.noreply.github.com>
2024-03-24 17:12:10 +0100

    Update Teclado.cs

--- Assets/Codigos/Teclado.cs
+++ Assets/Codigos/Teclado.cs
@@ -1,18 +1,16 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Teclado : MonoBehaviour {
-
     public InputField txtTexto;
     public GameObject Inicial;
-    bool mayusculas = false;
-    bool Paso = true;
+    private bool mayusculas = false;
+    private bool Paso = true;
 
-    public void OnEnable()
-    {
+    public void OnEnable() {
         EventSystem.current.SetSelectedGameObject(null);
         EventSystem.current.SetSelectedGameObject(Inicial);
 
@@ -21,10 +19,8 @@ public class Teclado : MonoBehaviour {
         Mayusculas();
     }
 
-    public void Update()
-    {
-        if (Input.GetAxis("joystick1_left_trigger") != 0 && Paso)
-        {
+    public void Update() {
+        if (Input.GetAxis("joystick1_left_trigger") != 0 && Paso) {
             Paso = false;
 
             Mayusculas();
@@ -32,89 +28,66 @@ public class Teclado : MonoBehaviour {
             StartCoroutine(SeguirPasando());
         }
 
-        if (Input.GetAxis("joystick1_right_trigger") != 0 && Paso)
-        {
+        if (Input.GetAxis("joystick1_right_trigger") != 0 && Paso) {
             Paso = false;
 
             Listo();
         }
 
-        if (Input.GetKeyDown(KeyCode.Joystick1Button2))
-        {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button2)) {
             Borrar();
         }
 
-        if (Input.GetKeyDown(KeyCode.Joystick1Button3) && txtTexto.text.Length <= 250)
-        {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button3) && txtTexto.text.Length <= 250) {
             txtTexto.text += " ";
         }
     }
 
-    private IEnumerator SeguirPasando()
-    {
+    private IEnumerator SeguirPasando() {
         yield return new WaitForSeconds(0.5f);
         Paso = true;
     }
 
-    public void BotonClick(string Letra)
-    {
-        if (txtTexto.text.Length <= 250)
-        {
-            if (mayusculas)
-            {
+    public void BotonClick(string Letra) {
+        if (txtTexto.text.Length <= 250) {
+            if (mayusculas) {
                 txtTexto.text += Letra.ToUpper();
-            }
-            else
-            {
+            } else {
                 txtTexto.text += Letra;
             }
         }
     }
 
-    public void Mayusculas()
-    {
+    public void Mayusculas() {
         mayusculas = !mayusculas;
 
-        if (mayusculas)
-        {
-            for (int i = 11; i < transform.childCount - 18; i++)
-            {
-
+        if (mayusculas) {
+            for (int i = 11; i < transform.childCount - 18; i++) {
                 transform.GetChild(i).GetComponentInChildren<Text>().text = transform.GetChild(i).GetComponentInChildren<Text>().text.ToUpper();
             }
-        }
-        else
-        {
-            for (int i = 11; i < transform.childCount - 18; i++)
-            {
+        } else {
+            for (int i = 11; i < transform.childCount - 18; i++) {
                 transform.GetChild(i).GetComponentInChildren<Text>().text = transform.GetChild(i).GetComponentInChildren<Text>().text.ToLower();
             }
         }
     }
 
-    public void Borrar()
-    {
-        if (txtTexto.text.Length > 0)
-        {
+    public void Borrar() {
+        if (txtTexto.text.Length > 0) {
             txtTexto.text = txtTexto.text.Substring(0, txtTexto.text.Length - 1);
         }
     }
 
-    public void Listo()
-    {
-        if (txtTexto.text.Trim() != "")
-        {
-            if (Controlador.instancia.EsNuevaCarpeta)
-            {
+    public void Listo() {
+        if (txtTexto.text.Trim() != "") {
+            if (Controlador.instancia.EsNuevaCarpeta) {
                 Controlador.instancia.NuevaCarpetaNombre = txtTexto.text;
                 Controlador.instancia.NuevaCarpetaAccion();
-            }
-            else
-            {
+            } else {
                 Controlador.instancia.TextoCambiado = txtTexto.text;
                 Controlador.instancia.RenombrarAccion();
             }
-            
+
             this.gameObject.SetActive(false);
         }
     }
